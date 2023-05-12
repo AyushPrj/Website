3d938ad4bbb8ee4ba3f98582be919548a91b2e6f
Ayush Praharaj <69159222+AyushPrj@users.noreply.github.com>
2023-05-12 04:01:02 +0300

    First Change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container, Row, Col, Button, Card } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fluid>
+        <Row className="justify-content-center">
+          <Col md={8}>
+            <Card className="text-center p-4">
+              <Card.Body>
+                <Card.Title>Welcome to Ayush's website!</Card.Title>
+                <Card.Text>This is a simple website that I created using React and Bootstrap.</Card.Text>
+                <Button variant="primary" href="#about">Learn more</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col md={4}>
+            <Card className="mb-3">
+              <Card.Img variant="top" src="https://source.unsplash.com/random/300x200?nature" />
+              <Card.Body>
+                <Card.Title>Nature</Card.Title>
+                <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
+                <Button variant="outline-secondary" href="#nature">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={4}>
+            <Card className="mb-3">
+              <Card.Img variant="top" src="https://source.unsplash.com/random/300x200?city" />
+              <Card.Body>
+                <Card.Title>City</Card.Title>
+                <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
+                <Button variant="outline-secondary" href="#city">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
